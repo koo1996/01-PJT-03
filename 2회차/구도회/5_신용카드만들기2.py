b55fd59f2adaeddd2714a50ce1d3db2446b877ed
koo1996 <koodo1996@gmail.com>
2022-08-01 10:18:59 +0900

    모의고사1 문제풀이 완료

--- 2회차/구도회/5_신용카드만들기2.py
+++ 2회차/구도회/5_신용카드만들기2.py
@@ -2,11 +2,16 @@ import sys
 
 sys.stdin = open("_신용카드만들기2.txt")
 
+
 for i in range(1,int(input())+1):
-    N = list(input())
-    count = 1
-    for j in range(len(N)):
-        if N[j] == '-':
-            result = result - count
+    N = input()
+    N = N.replace('-','') #'-' 제거
+    if N[0] == '3' or N[0] == '4' or N[0] == '5' or N[0] == '6' or N[0] == '9':
+        #3,4,5,6,9 유무
+        if len(N) == 16: #길이 16인가?
+            print('#{} {}'.format(i,'1')) 
         else:
-            print(len(N))
+            print('#{} {}'.format(i,'0'))
+            
+    else: 
+        print('#{} {}'.format(i,'0'))
--- 2회차/구도회/6_암호문1.py
+++ 2회차/구도회/6_암호문1.py
@@ -1,3 +1,31 @@
 import sys
 
 sys.stdin = open("_암호문1.txt")
+
+T = 10
+
+for t in range(1, T+1):
+    origin_list = int(input())
+    origin_list = list(map(int,input().split()))
+
+    command_len = int(input())
+    command_list = input().split()
+
+    #i의 초기화
+    i = 0
+
+    #while문 (반복문)
+    while i < len(command_list):
+        command = command_list[i]
+        if command == 'I':
+            #x,y 숫자리스트 s 구해야한다.
+            x = int(command_list[i+1])
+            y = int(command_list[i+2])
+            number_list = command_list[i+3:i+3+y]
+
+            #insert 메서드를 써서 x의 위치에 숫자들을 삽입한다.
+            #역순으로 삽입한다.
+            for number in number_list[::-1]:
+                origin_list.insert(x, int(number))
+        i = i + 1
+    print(f'#{t} ',*origin_list[:10])
